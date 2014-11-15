9dc14982c82f524daf546d81617c4a7c336a28dc
saxsir <saxsir.256@gmail.com>
2014-11-15 20:28:38 +0900

    change max num of user for test

--- app/final.php
+++ app/final.php
@@ -13,9 +13,9 @@ $app->get('/get_timeline',function() use($app) {
     // シンプルだけど遅いクエリ
     // cf. http://www.atmarkit.co.jp/news/201004/19/twitter.html
     // $sql = 'SELECT id, body FROM tweets WHERE user_id IN (SELECT source_id FROM follows WHERE destination_id = ?) ORDER BY created_at DESC LIMIT 20)';
-    $sql = 'SELECT id, body FROM tweets WHERE id IN (SELECT tweet_id  FROM timeline WHERE user_id = ?) ORDER BY created_at DESC LIMIT 20)';
+    $sql = 'SELECT id, body FROM tweets WHERE user_id IN (SELECT destination_id FROM followee WHERE source_id = ?) ORDER BY created_at DESC LIMIT 20';
     $sth = $con->prepare($sql);
-    $sth->execute(array(mt_rand(1, 100000)));
+    $sth->execute(array(mt_rand(1, 10000)));
     $results = $sth->fetchAll();
     return $app['twig']->render('get_timeline.twig',['tweets' => $results]);
 });
